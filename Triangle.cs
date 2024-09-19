50ec270e83e3ec4043292e406cc156f7ccd9185e
VENKATESHA G S <venki354119@gmail.com>
2024-09-19 23:07:05 +0530

    Print Stars

---
+++ Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Triangle
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the height of the triangle: ");
+        
+        int height = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= height; i++)
+        {
+            for (int j = height - i; j > 0; j--)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k < (2 * i - 1); k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
