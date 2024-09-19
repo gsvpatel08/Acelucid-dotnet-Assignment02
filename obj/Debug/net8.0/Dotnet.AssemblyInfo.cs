bce195e32099b6fa1a99fa208d654bd7f7d73134
VENKATESHA G S <venki354119@gmail.com>
2024-09-19 23:11:30 +0530

    Print Stars

--- obj/Debug/net8.0/Dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ec270e83e3ec4043292e406cc156f7ccd9185e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
